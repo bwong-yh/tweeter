cd3201300aecabfc7eb089f565da65905c54896d
Billy Wong <bwong.yh@protonmail.com>
2022-05-13 05:12:10 +0900

    create and implement renderTweets()

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,16 +5,35 @@
  */
 
 // data - remove later when implement ajax get
-const tweetData = {
-	user: {
-		name: "Newton",
-		avatars: "https://i.imgur.com/73hZDYK.png",
-		handle: "@SirIsaac"
+const data = [
+	{
+		user: {
+			name: "Newton",
+			avatars: "https://i.imgur.com/73hZDYK.png",
+			handle: "@SirIsaac"
+		},
+		content: {
+			text: "If I have seen further it is by standing on the shoulders of giants"
+		},
+		created_at: 1652210070252
 	},
-	content: {
-		text: "If I have seen further it is by standing on the shoulders of giants"
-	},
-	created_at: 1461116232227
+	{
+		user: {
+			name: "Descartes",
+			avatars: "https://i.imgur.com/nlhLi3I.png",
+			handle: "@rd"
+		},
+		content: {
+			text: "Je pense , donc je suis"
+		},
+		created_at: 1652296470252
+	}
+];
+
+const renderTweets = (data) => {
+	data.forEach((tweet) => {
+		$("#tweets-container").append(createTweetElement(tweet));
+	});
 };
 
 const createTweetElement = (tweetData) => {
@@ -25,21 +44,15 @@ const createTweetElement = (tweetData) => {
           <img class="avatar" src="${tweetData.user.avatars}" alt="">
           <p>${tweetData.user.name}</p>
         </div>
-        <div>
-          <p class="handle">${tweetData.user.handle}</p>
-        </div>
+        <p class="handle">${tweetData.user.handle}</p>
       </header>
       <p>${tweetData.content.text}</p>
       <footer>
-        <div>
-          <p>${new Date(tweetData.created_at)}</p>
-        </div>
-        <div><i class="fa-solid fa-flag"></i> <i class="fa-solid fa-retweet"></i> <i class="fa-solid fa-heart"></i></div>
+        <p>${new Date(tweetData.created_at)}</p>
+      <div><i class="fa-solid fa-flag"></i> <i class="fa-solid fa-retweet"></i> <i class="fa-solid fa-heart"></i></div>
       </footer>
     </article>
   `;
 };
-const tweet = createTweetElement(tweetData);
 
-console.log(tweet);
-$("#tweets-container").append(tweet);
+renderTweets(data);
