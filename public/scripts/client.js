5357b4754965de19b2fba99362a25721f2e904e5
Billy Wong <bwong.yh@protonmail.com>
2022-05-16 06:07:34 +0900

    implement ajax to fetch data and render it to the DOM

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,32 +4,6 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-// data - remove later when implement ajax get
-const data = [
-	{
-		user: {
-			name: "Newton",
-			avatars: "https://i.imgur.com/73hZDYK.png",
-			handle: "@SirIsaac"
-		},
-		content: {
-			text: "If I have seen further it is by standing on the shoulders of giants"
-		},
-		created_at: 1652210070252
-	},
-	{
-		user: {
-			name: "Descartes",
-			avatars: "https://i.imgur.com/nlhLi3I.png",
-			handle: "@rd"
-		},
-		content: {
-			text: "Je pense , donc je suis"
-		},
-		created_at: 1652296470252
-	}
-];
-
 const renderTweets = (data) => {
 	data.forEach((tweet) => {
 		$("#tweets-container").append(createTweetElement(tweet));
@@ -48,24 +22,32 @@ const createTweetElement = (tweetData) => {
     </header>
     <p>${tweetData.content.text}</p>
     <footer>
-      <p>${new Date(tweetData.created_at)}</p>
+      <p>${timeago.format(new Date(tweetData.created_at))}</p>
       <div><i class="fa-solid fa-flag"></i> <i class="fa-solid fa-retweet"></i> <i class="fa-solid fa-heart"></i></div>
     </footer>
   </article>
   `;
 };
 
-renderTweets(data);
+const loadTweets = () => {
+	$.get("/tweets", (data) => renderTweets(data));
+};
 
-// form handling
-$("form").submit(function(e) {
-	e.preventDefault();
+// calls loadTweets() & enables tweets posting when DOM is ready
+// https://stackoverflow.com/questions/65941258/why-jquery-ready-has-strikethrough
+$(function() {
+	loadTweets();
 
-	const text = $(this).children("#tweet-text").serialize();
+	// form handling
+	$("form").on("submit", function(e) {
+		e.preventDefault();
 
-	// sending post req
-	$.post("/tweets", text);
+		const text = $(this).children("#tweet-text").serialize();
 
-	// clear textarea
-	$(this).children("#tweet-text").val("");
+		// sending post req
+		$.post("/tweets", text);
+
+		// clear textarea
+		$(this).children("#tweet-text").val("");
+	});
 });
