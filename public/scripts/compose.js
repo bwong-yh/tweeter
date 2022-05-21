98b2b6cd2b5dd0529da447c6f18c7c51ccc9f796
Billy Wong <bwong.yh@protonmail.com>
2022-05-21 14:12:17 +0900

    refactor toggle form

--- public/scripts/compose.js
+++ public/scripts/compose.js
@@ -1,18 +1,21 @@
+const toggleForm = () => {
+	if ($(".new-tweet").hasClass("hide")) {
+		$(".new-tweet").slideUp(0, function() {
+			// slideUp is specifically for the first time clicking so that .new-tweet will not POP up, but slide down
+			$(this).removeClass("hide").slideDown("fast", function() {
+				$("#tweet-text").focus();
+			});
+		});
+	} else {
+		$(".new-tweet").slideUp("fast", function() {
+			$(this).addClass("hide");
+		});
+	}
+};
+
 $(function() {
-	// toggle form
 	$("nav div").on("click", function() {
-		if ($(".new-tweet").hasClass("hide")) {
-			$(".new-tweet").slideUp(0, function() {
-				// slideUp is specifically for the first time clicking so that .new-tweet will not POP up, but slide down
-				$(this).removeClass("hide").slideDown("fast", function() {
-					$("#tweet-text").focus();
-				});
-			});
-		} else {
-			$(".new-tweet").slideUp("fast", function() {
-				$(this).addClass("hide");
-			});
-		}
+		toggleForm();
 	});
 
 	// to-top-button is visible when window.scrollY > 400
@@ -24,5 +27,6 @@ $(function() {
 	// scroll to the top of the page when clickend
 	scrollBtn.on("click", function(e) {
 		$("html").animate({ scrollTop: 0 }, "slow");
+		toggleForm();
 	});
 });
