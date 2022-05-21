ec3486c0845001fa97da12bf467a82476d7132b4
Billy Wong <bwong.yh@protonmail.com>
2022-05-21 09:54:06 +0900

    implement button to scroll to the top of the page

---
+++ public/scripts/compose.js
@@ -0,0 +1,13 @@
+$(function() {
+	// button is visible when window.scrollY > 400
+	const scrollBtn = $(".container .to-top-btn");
+	$(document).on("scroll", function() {
+		window.scrollY < 400 ? $(scrollBtn).addClass("v-none") : $(scrollBtn).removeClass("v-none");
+	});
+
+	// scroll to the top of the page when clickend
+	scrollBtn.on("click", function(e) {
+		console.log("click");
+		$("html").animate({ scrollTop: 0 }, "slow");
+	});
+});
