629f2e9d09aa9eb1157b66d6921ac5821975d108
Billy Wong <bwong.yh@protonmail.com>
2022-05-21 13:58:09 +0900

    refactor toggle form

--- public/scripts/compose.js
+++ public/scripts/compose.js
@@ -4,18 +4,18 @@ $(function() {
 		if ($(".new-tweet").hasClass("hide")) {
 			$(".new-tweet").slideUp(0, function() {
 				// slideUp is specifically for the first time clicking so that .new-tweet will not POP up, but slide down
-				$(".new-tweet").removeClass("hide").slideDown("fast", function() {
+				$(this).removeClass("hide").slideDown("fast", function() {
 					$("#tweet-text").focus();
 				});
 			});
 		} else {
 			$(".new-tweet").slideUp("fast", function() {
-				$(".new-tweet").addClass("hide");
+				$(this).addClass("hide");
 			});
 		}
 	});
 
-	// button is visible when window.scrollY > 400
+	// to-top-button is visible when window.scrollY > 400
 	const scrollBtn = $(".container .to-top-btn");
 	$(document).on("scroll", function() {
 		window.scrollY < 400 ? $(scrollBtn).addClass("v-none") : $(scrollBtn).removeClass("v-none");
