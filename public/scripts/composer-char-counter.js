dbe417add457c7ca898c1ec567fafec7f5ce560f
Billy Wong <bwong.yh@protonmail.com>
2022-05-25 18:45:17 +0900

    modify textarea to wrap text dynamically

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -4,6 +4,9 @@ $(function() {
 		const char = $(this).val();
 		const remainChar = 140 - char.length;
 
+		// set height to equal the scrollHeight to "wrap" text
+		this.style.height = `${this.scrollHeight}px`;
+
 		$(this).siblings().children("#char-counter").text(remainChar).toggleClass("error", remainChar < 0);
 	});
 });
