c704d139f6e3556c9cc2d7788f1aa9e871e71330
Billy Wong <bwong.yh@protonmail.com>
2022-05-26 02:00:11 +0900

    update textarea spacing

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -6,6 +6,7 @@ $(function() {
 
 		// set height to equal the scrollHeight to "wrap" text
 		this.style.height = `${this.scrollHeight}px`;
+		this.style.paddingBottom = "0.5rem";
 
 		$(this).siblings().children("#char-counter").text(remainChar).toggleClass("error", remainChar < 0);
 	});
